80c716afde4c30431ddc47ad31759f5121d3c9e2
Nijumdip <nijumdip019911@gmail.com>
2023-01-17 12:04:25 +0600

    added bottom nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import BottomNavigation from './components/Navigation/BottomNavigation';
+import Home from './components/Home/Home';
 import TopNavigation from './components/Navigation/TopNavigation';
 
 function App() {
   return (
     <div className="App">
       < TopNavigation />
-      <BottomNavigation />
+      <Home/>
     </div>
   );
 }
--- src/components/Navigation/TopNavigation.js
+++ src/components/Navigation/TopNavigation.js
@@ -1,85 +1,156 @@
 import React from "react";
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const TopNavigation = () => {
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="/">
-                    Navbar
-                </a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <nav class="navbar navbar-expand-lg bg-light ">
+                <div class="container-fluid mx-5">
+                    <a class="navbar-brand" href="/">
+                        Electro-ware
+                    </a>
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/">
+                                    My Account
+                                </a>
+                            </li>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/">
-                                Home <span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">
-                                Link
-                            </a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a
-                                class="nav-link dropdown-toggle"
-                                href="/"
-                                id="navbarDropdown"
-                                role="button"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                                Dropdown
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="/">
-                                    Action
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/">
+                                    Wishlist
                                 </a>
-                                <a class="dropdown-item" href="/">
-                                    Another action
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="/">
+                                    My Cart
                                 </a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="/">
-                                    Something else here
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="/">
+                                    Checkout
                                 </a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="/">
-                                Disabled
-                            </a>
-                        </li>
-                    </ul>
-                    <form class="form-inline my-2 my-lg-0">
-                        <input
-                            class="form-control mr-sm-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-                            Search
-                        </button>
-                    </form>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="/">
+                                    login
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="d-flex">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                              Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                              Separated link
+                            </NavDropdown.Item>
+                          </NavDropdown>
+                                
+                                <li class="nav-item dropdown">
+                                    <a
+                                        class="nav-link dropdown-toggle"
+                                        href="/"
+                                        role="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >
+                                        currency :USD
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item" href="/">
+                                                USD
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="/">
+                                                INR
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="/">
+                                                GPB
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                    <a
+                                        class="nav-link dropdown-toggle"
+                                        href="/"
+                                        role="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >
+                                        language :English
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a class="dropdown-item" href="/">
+                                                USD
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="/">
+                                                INR
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="/">
+                                                GPB
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </nav>
-            <div class="container-fluid py-3">
-                <div class="row">
-                    <div class="col"></div>
-                </div>
-            </div>
+
+            
+            <p className="text-start my-4 mx-5 p-4">Or right-aligned</p>
+            <Nav className="justify-content-start mx-5" activeKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="/home">Active</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">Link</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2">Link</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="disabled" disabled>
+                        Disabled
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
         </>
     );
 };
