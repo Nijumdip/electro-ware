16ed6117af55df8dffade6ce7f824bcbb69b1e8b
Nijumdip <nijumdip019911@gmail.com>
2023-01-17 22:08:45 +0600

    almost done nav

--- src/components/Navigation/TopNavigation.js
+++ src/components/Navigation/TopNavigation.js
@@ -1,15 +1,14 @@
 import React from "react";
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Card from "react-bootstrap/Card";
 
 const TopNavigation = () => {
     return (
         <>
             <nav class="navbar navbar-expand-lg bg-light ">
                 <div class="container-fluid mx-5">
-                    <a class="navbar-brand" href="/">
+                    <a class="navbar-brand d-lg-none " href="/">
                         Electro-ware
                     </a>
                     <button
@@ -56,100 +55,61 @@ const TopNavigation = () => {
                             </li>
                         </ul>
                         <div class="d-flex">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                              Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                              Separated link
-                            </NavDropdown.Item>
-                          </NavDropdown>
-                                
-                                <li class="nav-item dropdown">
-                                    <a
-                                        class="nav-link dropdown-toggle"
-                                        href="/"
-                                        role="button"
-                                        data-bs-toggle="dropdown"
-                                        aria-expanded="false"
-                                    >
-                                        currency :USD
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a class="dropdown-item" href="/">
-                                                USD
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="/">
-                                                INR
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="/">
-                                                GPB
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
+                            <div className="mx-3">
+                                <NavDropdown title="currency :USD" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">USD</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">INR</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">GPB</NavDropdown.Item>
+                                </NavDropdown>
+                            </div>
 
-                                <li class="nav-item dropdown">
-                                    <a
-                                        class="nav-link dropdown-toggle"
-                                        href="/"
-                                        role="button"
-                                        data-bs-toggle="dropdown"
-                                        aria-expanded="false"
-                                    >
-                                        language :English
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a class="dropdown-item" href="/">
-                                                USD
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="/">
-                                                INR
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="/">
-                                                GPB
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
+                            <div className="mx-3">
+                                <NavDropdown title="language :English " id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">English</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">French</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">German</NavDropdown.Item>
+                                </NavDropdown>
+                            </div>
                         </div>
                     </div>
                 </div>
             </nav>
 
-            
-            <p className="text-start my-4 mx-5 p-4">Or right-aligned</p>
+            <div className="text-start mx-5 p-1 ">
+                <Card.Img
+                    style={{ height: "150px", width: "150px" }}
+                    className="rounded-circle "
+                    variant="top"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1qIwph8ov6iK7ldfl_lmj_HbFxsNDW0o3vA&usqp=CAU"
+                />
+            </div>
+
             <Nav className="justify-content-start mx-5" activeKey="/home">
+               
                 <Nav.Item>
-                    <Nav.Link href="/home">Active</Nav.Link>
+                    <Nav className="px-4" href="/home">HOME</Nav>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="link-1">Link</Nav.Link>
+                    <Nav className="px-4" href="link-1">SHOP PAGE</Nav>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="link-2">Link</Nav.Link>
+                    <Nav className="px-4" href="link-2">Single product</Nav>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="disabled" disabled>
-                        Disabled
-                    </Nav.Link>
+                    <Nav className="px-4" href="disabled">Cart</Nav>
                 </Nav.Item>
+                <Nav.Item>
+                    <Nav className="px-4" href="disabled">Checkout</Nav>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav className="px-4" href="disabled">Category</Nav>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav className="px-4" href="disabled">Others</Nav>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav className="px-4" href="disabled">Contact</Nav>
+                </Nav.Item>            
             </Nav>
         </>
     );
